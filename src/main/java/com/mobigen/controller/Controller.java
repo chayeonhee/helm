5c4e2ab352eb22b539720724e4959b9be8187e7d
cha <yeonhee1021@gmail.com>
2025-03-07 15:23:47 +0900

    test

--- src/main/java/com/mobigen/controller/Controller.java
+++ src/main/java/com/mobigen/controller/Controller.java
@@ -1,15 +1,19 @@
 package com.mobigen.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 
+
 @RestController
-@RequestMapping(value = "/api",method = RequestMethod.GET)
+@RequestMapping(value = "/",method = RequestMethod.GET)
 public class Controller {
-    
+    @Value("${MESSAGE}")
+    private String uiMessage;
+
     @GetMapping("/hi")
     public String getMethodName() {
-        return "Hello World!!!";
+        return uiMessage;
     }
 }
